2025dbb986df1845137e9af43fbd0a0449625c8b
Arcanox <admin@arcanox.me>
2020-04-23 22:32:47 -0500

    Fixed translations

--- src/main/kotlin/me/arcanox/techmod/ItemGroup.kt
+++ src/main/kotlin/me/arcanox/techmod/ItemGroup.kt
@@ -5,6 +5,6 @@ import me.arcanox.techmod.api.Constants
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack;
 
-object ItemGroup : ItemGroup(TechMod.Name) {
+object ItemGroup : ItemGroup(TechMod.ModID) {
 	override fun createIcon(): ItemStack = API.getInstance().blocks().getBlockItemStack(Constants.Blocks.AutomaticDoor, 1);
 }
